412f27582a1c53834a2a4854b263d8dc256f87df
llsoyeonll <lsoyeonl2000@gmail.com>
2026-09-11 18:49:00 +0300

    Add files via upload

---
+++ forum/supabase-config.js
@@ -0,0 +1,11 @@
+window.NINEYIN_CONFIG = {
+  version: '0.0.22',
+  url: 'https://hwokqidewryewohdrwow.supabase.co',
+  anonKey: 'sb_publishable_R5JVuXX59SnIpunEOsIYMA_K2FakDQs',
+  demoMode: false,
+  storagePrefix: 'nineyin-community',
+  defaultAvatar: 'default-profile.webp',
+  brandIcon: 'guide-brand-mark.webp',
+  guideUrl: '../index.html',
+  forumUrl: 'index.html'
+};
